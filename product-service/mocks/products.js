29e7514cc711e4ad0ae22a720012abad68bdcd7e
Alexandra Dovlyash <dovlyashalexandra@gmail.com>
2022-10-10 19:11:12 +0200

    [Task 3] Add lambdas to get mocked products list and product by id

---
+++ product-service/mocks/products.js
@@ -0,0 +1,28 @@
+export async function productsList() {
+    return [
+        {
+            id: '68375e0a-fe52-45b9-bf76-bc2e35cbb089',
+            title: 'Brit Turkey & Oat',
+            image: 'https://zooshop-images.s3.amazonaws.com/products/Brit_Turkey_Oat.jpeg',
+            price: 58,
+        },
+        {
+            id: '974889e0-4ed0-4ebd-8fd2-e50cc45bbac0',
+            title: 'Brit Salmon & Potato',
+            image: 'https://zooshop-images.s3.amazonaws.com/products/Brit_Salmon_Potato.jpeg',
+            price: 62.3,
+        },
+        {
+            id: '74dc16eb-c191-4a1c-aa59-a94cffef406b',
+            title: 'Brit Lamb & Rice',
+            image: 'https://zooshop-images.s3.amazonaws.com/products/Brit_Salmon_Potato.jpeg',
+            price: 49.67,
+        },
+        {
+            id: '144a131b-c2c3-4784-acd9-10dd69d4df63',
+            title: 'Brit Insect & Fish',
+            image: 'https://zooshop-images.s3.amazonaws.com/products/Brit_Insect_Fish.webp',
+            price: 52,
+        },
+    ]
+}
