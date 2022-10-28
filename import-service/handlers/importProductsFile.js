74654354f8d4f7a94f8ac71ccdd4033ebaff6e01
Alexandra Dovlyash <dovlyashalexandra@gmail.com>
2022-10-28 22:58:07 +0200

    [Task 5] Add import service

---
+++ import-service/handlers/importProductsFile.js
@@ -0,0 +1,45 @@
+'use strict';
+import AWS from 'aws-sdk';
+
+export const importProductsFile = async (event) => {
+    console.log(
+        '[Import Products File lambda] incoming request, event:',
+        event,
+    );
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    };
+
+    try {
+        const fileName = event?.queryStringParameters?.name;
+        const s3 = new AWS.S3({
+            region: 'us-east-1',
+        });
+        const filePath = `uploaded/${fileName}`;
+
+        const params = {
+            Bucket: 'zooshop-import',
+            Key: filePath,
+            Expires: 60,
+            ContentType: 'text/csv',
+        };
+
+        const signedUrl = await s3.getSignedUrlPromise('putObject', params);
+        response.body = signedUrl;
+
+        return response;
+    } catch (err) {
+        console.error('error', err);
+
+        response.statusCode = 500;
+        response.body = JSON.stringify({
+            message: 'Internal error',
+        });
+
+        return response;
+    }
+};
